05d63e27392eba917b0af7db71faae97054ea169
Jan Houben <jan@nexttrex.de>
2017-02-06 22:28:35 +0100

    made some i3bar elements clickable

--- panel/conky-i3bar.sh
+++ panel/conky-i3bar.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+panelColor="#30353a"
+panelSecondColor="#3daee9"
+dzenArgs="-y 20 -fg #6c6c6c -bg $panelColor -fn xft:Terminus:size=10 -p -ta c -e onstart=uncollapse;button1=exit;button3=exit"
 
 # Send the header so that i3bar knows we want to use JSON:
-echo '{"version":1}'
+echo '{"version" : 1, "click_events": true }'
 
 # Begin the endless array.
 echo '['
@@ -10,4 +13,46 @@ echo '['
 echo '[],'
 
 # Now send blocks with information forever:
-exec conky -c ~/.config/i3/panel/conkyrc
+exec conky -c ~/.config/i3/panel/conkyrc &
+
+
+# read click events
+while read; do
+    # extract name of clicked element
+    name=$(echo $REPLY | sed -re 's/.*"name":"([^"]*)".*/\1/')
+    x=$(echo $REPLY | sed -re 's/.*"x":([^,]*),.*/\1/')
+    killall dzen2
+    case $name in
+        "date")
+            calX=$(expr $x - 80)
+            TODAY=$(expr `date +'%d'` + 0)
+            MONTH=`date +'%m'`
+            YEAR=`date +'%Y'`
+
+            (
+            echo "^bg($panelColor)^fg(#ffffff) Datum";
+
+            # current month, hilight header and today
+            cal | sed -re "s/^(.*[A-Za-z][A-Za-z]*.*)$/^fg($panelSecondColor)^bg($panelColor)\1/;
+            s/(^|[ ])($TODAY)($|[ ])/\1^bg($panelSecondColor)^fg($panelColor)\2^fg(#6c6c6c)^bg($panelColor)\3/"
+            ) | dzen2 $dzenArgs -x $calX -w 160 -l 7 &
+            ;;
+        "weather")
+             chromium "http://www.accuweather.com/de/de/trier/54290/hourly-weather-forecast/171045?lang=de" &> /dev/null &
+             ;;
+        "updates")
+            konsole -e "bash -c 'sudo pacman -Syu; read -rsp $\"Ende...\" key'"
+            ;;
+        "space")
+            filelight /home &> /dev/null &
+            ;;
+        "temp")
+            tempX=$(expr $x - 100)
+            (
+                echo "^bg($panelColor)^fg(#ffffff) Temperaturen";
+                # strip min, low and high values
+                sensors |  sed -r 's/([^\(]*).*/\1/'
+            ) | dzen2 $dzenArgs -l 20 -x $tempX -w 200 &
+            ;;
+    esac
+done
