25e725b60822d32ee74cfb0660320efba58ec831
Jan Houben <next-play@web.de>
2017-02-07 03:29:55 +0100

    added readme + setup

---
+++ README.md
@@ -0,0 +1,12 @@
+## Installation
+
+1. Clone this repository into .config
+```
+git clone git@git.et.tc:Jan/i3-config.git ~/.config/i3
+cd ~/.config/i3
+```
+2. Run setup.sh
+```
+./setup.sh
+```
+3. start i3
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install dependencies
+sudo pacman -Sy --needed\
+ i3-wm i3lock\
+ conky ttf-font-awesome filelight dzen2 playerctl pamixer \
+ feh compton albert network-manager-applet parcellite volnoti redshift
+
+# generate config
+./generateConfig.sh
