f82ff9f6a8d2935c7aad82eb9765bd2350537a52
Jan Houben <next-play@web.de>
2017-02-07 11:53:21 +0100

    added xautolock to setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # install dependencies
 sudo pacman -Sy --needed\
- i3-wm i3lock\
+ i3-wm i3lock xautolock\
  conky ttf-font-awesome filelight dzen2 playerctl pamixer \
  feh compton albert network-manager-applet parcellite volnoti redshift
 
