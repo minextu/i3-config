1c3469c1a7f8ea89a687dabfb81b20708d387646
Jan Houben <next-play@web.de>
2017-02-09 01:09:20 +0100

    added laptop power saving + lid closing handling

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 sudo pacman -Sy --needed\
  i3-wm i3lock xautolock notify-osd\
  conky ttf-font-awesome numix-icon-theme-git filelight dzen2 playerctl pamixer\
- feh compton albert network-manager-applet parcellite volnoti redshift
+ feh compton albert network-manager-applet parcellite volnoti redshift\
+ upower tlp
 
 # generate config
 ./generateConfig.sh
