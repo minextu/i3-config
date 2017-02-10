978886033cb80bdf897e3930f90ae929ce684bfe
Jan Houben <next-play@web.de>
2017-02-10 14:29:49 +0100

    removed terminal autostart

--- config.d/08optionalAutostart.sh
+++ config.d/08optionalAutostart.sh
@@ -1,11 +1,7 @@
 # Autostart desktop programs
 exec chromium
-exec --no-startup-id i3-msg "workspace $workspace9; exec konsole"
 exec dolphin
 exec yakuake
 
 # touchpad
 exec --no-startup-id synclient TapButton1=1 TapButton2=3 TapButton3=2
-
-# switch to workspace 1 again
-exec --no-startup-id sleep 2; i3-msg workspace $workspace1
