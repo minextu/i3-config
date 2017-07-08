7e252d1bf972569a78ce2cd2a893cad9eb245242
Jan Houben <jan@et.tc>
2017-07-08 22:40:08 +0200

    disable wacom tablet for now

--- config.d/08optionalAutostart.sh
+++ config.d/08optionalAutostart.sh
@@ -1,12 +1,12 @@
 # wacom tablet
-exec_always --no-startup-idxsetwacom --set "Wacom Intuos PT M Pen stylus" Area 100 100 7045 3963
-exec_always --no-startup-idxsetwacom set "Wacom Intuos PT M Pen stylus" MapToOutput $mainMonitor
-exec_always --no-startup-idxsetwacom set "Wacom Intuos PT M Pen stylus" Button 1
-exec_always --no-startup-idxsetwacom set "Wacom Intuos PT M Pen stylus" Button 2 "button 3"
-exec_always --no-startup-idxsetwacom set "Wacom Intuos PT M Pen stylus" Button 3 "button 3"
-exec_always --no-startup-idxsetwacom set "Wacom Intuos PT M Finger touch" touch off
-exec_always --no-startup-idxsetwacom set "Wacom Intuos PT M Pad pad" Button 1 "key +XF86AudioLowerVolume"
-exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Pad pad" Button 3 "key +XF86AudioRaiseVolume"
+#exec_always --no-startup-id xsetwacom --set "Wacom Intuos PT M Pen stylus" Area 100 100 7045 3963
+#exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Pen stylus" MapToOutput $mainMonitor
+#exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Pen stylus" Button 1
+#exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Pen stylus" Button 2 "button 3"
+#exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Pen stylus" Button 3 "button 3"
+#exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Finger touch" touch off
+#exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Pad pad" Button 1 "key +XF86AudioLowerVolume"
+#exec_always --no-startup-id xsetwacom set "Wacom Intuos PT M Pad pad" Button 3 "key +XF86AudioRaiseVolume"
 
 # Autostart desktop programs
 exec firefox
